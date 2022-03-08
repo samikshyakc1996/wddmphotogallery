4e39a8ef9315847e55ab6abc3dae8796483b6b08
Ronak Sakhuja <ronak.sakhuja@article.com>
2022-03-07 21:49:58 -0800

    Project upload

---
+++ js/index.js
@@ -0,0 +1,120 @@
+
+$(document).ready(function(){
+    // $(".signin_link").dis
+
+    // if(localStorage.getItem("uid"))
+    //     getImages()
+    // else{
+    //     window.location.href = '/login.html'; 
+
+    // }
+})
+var uid;
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      getImages()
+      // ...
+    } else {
+      // User is signed out
+      window.location.href = '/login.html'; 
+
+      // ...
+    }
+  });
+  var  db = firebase.firestore(app);
+  var storage = firebase.storage(app);
+
+function submitImage(){
+    var file=document.getElementById("files").files[0];
+    var storageref=storage.ref();
+    var name=document.getElementById("fileName").value
+    var uploadTask=storageref.child("images").child(name).put(file);
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+    }, 
+    (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+        case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            break;
+        case 'storage/canceled':
+            // User canceled the upload
+            break;
+
+        // ...
+
+        case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+    }, 
+    () => {
+        // Upload completed successfully, now we can get the download URL
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log('File available at', downloadURL);
+        var userid = firebase.auth().currentUser.uid
+        db.collection("users").doc(userid).collection("images").doc().set({
+            imageName: name,
+            imageUrl: downloadURL
+        })
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+        });
+    }
+    );
+}
+
+$(".submitimage").on("click",function(e){
+    submitImage()
+})
+
+function getImages(){
+    var uid =firebase.auth().currentUser.uid
+    var userRef = db.collection("users").doc(uid);
+    userRef.collection("images").get()
+    .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+            console.log(doc.id, " => ", doc.data());
+            createImageList(doc.data().imageUrl,doc.data().imageName);
+        });
+    });
+}
+
+function createImageList(url, name){
+    var x='<div class="box"><div class="boxInner"><img src="'+url+'"/><div class="titleBox">'+name+'</div></div></div>';
+    $(".wrap").append(x)
+}
+function logout(){
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+
+    }).catch((error) => {
+        // An error happened.
+        console.log(error)
+      });
+}
+$(".logout").on("click", function(e){
+    e.preventDefault()
+    logout()
+})
---
+++ js/login.js
@@ -0,0 +1,77 @@
+var  db = firebase.firestore(app);
+var storage = firebase.storage(app);
+function signup(email,password){
+    console.log("attempt to signup")
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        console.log(user);
+        db.collection("users").doc(user.uid).set({
+            email: user.email,
+        })
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+    })
+    .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage)
+        // ..
+    });
+}
+function login(email,password){
+    console.log("attempt to login")
+    firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    localStorage.setItem('uid', user.uid);
+    window.location.href = '/gallery.html'; 
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+}
+
+$("#submitbutton").on("click",function(e){
+
+    e.preventDefault()
+    var email = $("input[name='email']")[0].value
+    var password = $("input[name='password']")[0].value
+    if($("#submitbutton").hasClass("login")) login(email,password)
+    else signup(email,password)
+});
+
+$("#signupButton").on("click",function(e){
+    e.preventDefault();
+    if($("#signupButton").hasClass("loginbut")){
+        $(".action")[0].innerText="Login"
+        $(".action").removeClass("signupaction")
+        $(".action").addClass("loginaction")
+        $(".signup_link p")[0].innerText = "Not a Member?"
+        $(".signup_link a")[0].innerText = "Signup"
+        $(".signup_link a").removeClass("loginbut")
+        $(".signup_link a").addClass("signupbut")
+        $("#submitbutton").removeClass("signup");
+        $("#submitbutton").addClass("login");
+    }
+    else{
+        $(".action")[0].innerText="Sign up"
+        $(".action").addClass("signupaction")
+        $(".action").removeClass("loginaction")
+        $(".signup_link p")[0].innerText = "Already a member?"
+        $(".signup_link a")[0].innerText = "Login"
+        $(".signup_link a").removeClass("signupbut")
+        $(".signup_link a").addClass("loginbut")
+        $("#submitbutton").addClass("signup");
+        $("#submitbutton").removeClass("login");
+    }
+});
