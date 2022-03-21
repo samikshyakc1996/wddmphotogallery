ba88038982409e2b6139ae8f43967ffbfff7e0b3
samikshyakc1996 <samikshyakc1996@gmail.com>
2022-03-21 15:16:10 -0400

    delete functionality added

--- js/index.js
+++ js/index.js
@@ -1,122 +1,175 @@
-
-$(document).ready(function(){
-    // $(".signin_link").dis
-
-    // if(localStorage.getItem("uid"))
-    //     getImages()
-    // else{
-    //     window.location.href = '/login.html'; 
-
-    // }
-})
+$(document).ready(function () {
+  // $(".signin_link").dis
+  // if(localStorage.getItem("uid"))
+  //     getImages()
+  // else{
+  //     window.location.href = '/login.html';
+  // }
+});
 var uid;
 firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      var uid = user.uid;
-      getImages()
-      // ...
-    } else {
-      // User is signed out
-      window.location.href = '/login.html'; 
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var uid = user.uid;
+    getImages();
+    // ...
+  } else {
+    // User is signed out
+    window.location.href = "/login.html";
 
-      // ...
-    }
-  });
-  var  db = firebase.firestore(app);
-  var storage = firebase.storage(app);
+    // ...
+  }
+});
+var db = firebase.firestore(app);
+var storage = firebase.storage(app);
 
-function submitImage(){
-    var file=document.getElementById("files").files[0];
-    var storageref=storage.ref();
-    var name=document.getElementById("fileName").value
-    var uploadTask=storageref.child("images").child(name).put(file);
-    // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+function submitImage() {
+  var file = document.getElementById("files").files[0];
+  fileName = file.name.split(".").shift();
+  extension = file.name.split(".").pop();
+  const id = db.collection("Images").doc().id;
+  var storageRef = storage.ref(`images/${id}.${extension}`);
+
+  var name = document.getElementById("fileName").value;
+  // var uploadTask = storageRef.child("images").child(name).put(file);
+  const uploadTask = storageRef.put(file);
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on(
+    firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
     (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+      switch (snapshot.state) {
         case firebase.storage.TaskState.PAUSED: // or 'paused'
-            console.log('Upload is paused');
-            break;
+          console.log("Upload is paused");
+          break;
         case firebase.storage.TaskState.RUNNING: // or 'running'
-            console.log('Upload is running');
-            break;
-        }
-    }, 
+          console.log("Upload is running");
+          break;
+      }
+    },
     (error) => {
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-        case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break;
-        case 'storage/canceled':
-            // User canceled the upload
-            break;
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case "storage/unauthorized":
+          // User doesn't have permission to access the object
+          break;
+        case "storage/canceled":
+          // User canceled the upload
+          break;
 
         // ...
 
-        case 'storage/unknown':
-            // Unknown error occurred, inspect error.serverResponse
-            break;
-        }
-    }, 
+        case "storage/unknown":
+          // Unknown error occurred, inspect error.serverResponse
+          break;
+      }
+    },
     () => {
-        // Upload completed successfully, now we can get the download URL
-        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
-        console.log('File available at', downloadURL);
-        var userid = firebase.auth().currentUser.uid
-        db.collection("users").doc(userid).collection("images").doc().set({
+      // Upload completed successfully, now we can get the download URL
+      uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log("File available at", downloadURL);
+        var userid = firebase.auth().currentUser.uid;
+        db.collection("users")
+          .doc(userid)
+          .collection("images")
+          .doc(id)
+          .set({
             imageName: name,
-            imageUrl: downloadURL
-        })
-        .then(() => {
+            imageUrl: downloadURL,
+            id,
+          })
+          .then(() => {
             console.log("Document successfully written!");
-            location.reload()
-        })
-        .catch((error) => {
+            location.reload();
+          })
+          .catch((error) => {
             console.error("Error writing document: ", error);
-        });
-        });
+          });
+      });
     }
-    );
+  );
 }
 
-$(".submitimage").on("click",function(e){
-    submitImage()
+$(".submitimage").on("click", function (e) {
+  submitImage();
+});
 
-})
-
-function getImages(){
-    var uid =firebase.auth().currentUser.uid
-    var userRef = db.collection("users").doc(uid);
-    userRef.collection("images").get()
-    .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-            console.log(doc.id, " => ", doc.data());
-            createImageList(doc.data().imageUrl,doc.data().imageName);
-        });
+function getImages() {
+  var uid = firebase.auth().currentUser.uid;
+  var userRef = db.collection("users").doc(uid);
+  userRef
+    .collection("images")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // console.log(doc.id, " => ", doc.data());
+        createImageList(doc.data().imageUrl, doc.data().imageName);
+      });
     });
 }
 
-function createImageList(url, name){
-    var x='<div class="box"><div class="boxInner"><img src="'+url+'"/><div class="titleBox">'+name+'</div></div></div>';
-    $(".wrap").append(x)
-}
-function logout(){
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
+function createImageList(url, name) {
+  var x =
+    '<div class="box"><div class="boxInner"><img src="' +
+    url +
+    '"/><span class="deleteBtn">X</span><div class="titleBox">' +
+    name +
+    "</div></div></div>";
 
-    }).catch((error) => {
-        // An error happened.
-        console.log(error)
-      });
+  $(".wrap").append(x);
+  const deleteBtn = document.querySelectorAll(".deleteBtn");
+  // console.log("deleteBtn:", deleteBtn);
+
+  Array.from(deleteBtn).forEach((item) => {
+    // console.log("item", item);
+    item.addEventListener("click", function () {
+      var uid = firebase.auth().currentUser.uid;
+      console.log("uid: is a userid", uid);
+      var userRef = db.collection("users").doc(uid);
+      console.log("userref", userRef);
+      let pictureRef = storage.refFromURL(url);
+      // let checkWhatItReturns = userRef
+      //   .collection("images")
+      //   .doc("RHqmRbN147sioZPsR6ow");
+      console.log("pictureRef: ", pictureRef);
+      console.log("pictureRef.name", pictureRef.name);
+      pictureRef
+        .delete()
+        .then(() => {
+          console.log("deleted");
+          db.collection("users")
+            .doc(uid)
+            .collection("images")
+            .doc(pictureRef.name.split(".").shift())
+            .delete()
+            .then(() => {
+              console.log("document deleted successfully from db");
+              location.reload();
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch(() => console.log("error deleting"));
+    });
+  });
 }
-$(".logout").on("click", function(e){
-    e.preventDefault()
-    logout()
-})
+
+function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+      console.log(error);
+    });
+}
+$(".logout").on("click", function (e) {
+  e.preventDefault();
+  logout();
+});
--- js/login.js
+++ js/login.js
@@ -1,82 +1,102 @@
-var  db = firebase.firestore(app);
+var db = firebase.firestore(app);
 var storage = firebase.storage(app);
-function signup(email,password){
-    console.log("attempt to signup")
+const errorMsg = document.getElementById("errorMessage");
+const errorMsgSI = document.getElementById("errorMessageSI");
+function signup(email, password) {
+  console.log("attempt to signup");
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
-        // Signed in 
-        var user = userCredential.user;
-        console.log(user);
-        db.collection("users").doc(user.uid).set({
-            email: user.email,
+      // Signed in
+      var user = userCredential.user;
+      console.log(user);
+      db.collection("users")
+        .doc(user.uid)
+        .set({
+          email: user.email,
         })
         .then(() => {
-            console.log("Document successfully written!");
-            login(email,password)
+          console.log("Document successfully written!");
+          login(email, password);
         })
         .catch((error) => {
-            console.error("Error writing document: ", error);
+          console.error("Error writing document: ", error);
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          //   if (errorCode === "auth/email-already-in-use") {
+          //     errorMsgSI.innerHTML = "The email address is already in use ";
+          //     errorMsgSI.style.display = "block";
+          //   } else if (
+          //     errorCode == "auth/weak-password" ||
+          //     errorMessage == "auth/weak-password"
+          //   ) {
+          //     // alert("The password is too weak.");
+          //     errorMsgSI.innerHTML = "The password is too weak.";
+          //     errorMsgSI.style.display = "block";
+          //   } else {
+          //     // alert(errorMessage);
+          //     errorMsgSI.innerHTML = errorMessage;
+          //     errorMsgSI.style.display = "block";
+          //   }
+
+          console.log(error);
         });
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorMessage)
-        // ..
     });
 }
-function login(email,password){
-    console.log("attempt to login")
-    firebase.auth().signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    // Signed in
-    var user = userCredential.user;
-    localStorage.setItem('uid', user.uid);
-    window.location.href = '/gallery.html'; 
-    // ...
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+
+function login(email, password) {
+  console.log("attempt to login");
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      localStorage.setItem("uid", user.uid);
+      window.location.href = "/gallery.html";
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      errorMsg.innerHTML = "Invalid Email or password. ";
+      errorMsg.style.display = "block";
+    });
 }
 
-$("#submitbutton").on("click",function(e){
-
-    e.preventDefault()
-    var email = $("input[name='email']")[0].value
-    var password = $("input[name='password']")[0].value
-    if($("#submitbutton").hasClass("login")) login(email,password)
-    else signup(email,password)
+$("#submitbutton").on("click", function (e) {
+  e.preventDefault();
+  var email = $("input[name='email']")[0].value;
+  var password = $("input[name='password']")[0].value;
+  if ($("#submitbutton").hasClass("login")) login(email, password);
+  else signup(email, password);
 });
 
-$("#signupButton").on("click",function(e){
-    e.preventDefault();
-    if($("#signupButton").hasClass("loginbut")){
-        $(".action")[0].innerText="Login"
-        $(".action").removeClass("signupaction")
-        $(".action").addClass("loginaction")
-        $(".signup_link p")[0].innerText = "Not a Member?"
-        $(".signup_link a")[0].innerText = "Signup"
-        $(".signup_link a").removeClass("loginbut")
-        $(".signup_link a").addClass("signupbut")
-        $("#submitbutton").removeClass("signup");
-        $("#submitbutton").addClass("login");
-        $("#submitbutton")[0].value="Login"
-
-    }
-    else{
-        $(".action")[0].innerText="Sign up"
-        $(".action").addClass("signupaction")
-        $(".action").removeClass("loginaction")
-        $(".signup_link p")[0].innerText = "Already a member?"
-        $(".signup_link a")[0].innerText = "Login"
-        $(".signup_link a").removeClass("signupbut")
-        $(".signup_link a").addClass("loginbut")
-        $("#submitbutton").addClass("signup");
-        $("#submitbutton").removeClass("login");
-        $("#submitbutton")[0].value="Sign Up"
-
-    }
+$("#signupButton").on("click", function (e) {
+  e.preventDefault();
+  if ($("#signupButton").hasClass("loginbut")) {
+    $(".action")[0].innerText = "Login";
+    $(".action").removeClass("signupaction");
+    $(".action").addClass("loginaction");
+    $(".signup_link p")[0].innerText = "Not a Member?";
+    $(".signup_link a")[0].innerText = "Signup";
+    $(".signup_link a").removeClass("loginbut");
+    $(".signup_link a").addClass("signupbut");
+    $("#submitbutton").removeClass("signup");
+    $("#submitbutton").addClass("login");
+    $("#submitbutton")[0].value = "Login";
+  } else {
+    $(".action")[0].innerText = "Sign up";
+    $(".action").addClass("signupaction");
+    $(".action").removeClass("loginaction");
+    $(".signup_link p")[0].innerText = "Already a member?";
+    $(".signup_link a")[0].innerText = "Login";
+    $(".signup_link a").removeClass("signupbut");
+    $(".signup_link a").addClass("loginbut");
+    $("#submitbutton").addClass("signup");
+    $("#submitbutton").removeClass("login");
+    $("#submitbutton")[0].value = "Sign Up";
+  }
 });
